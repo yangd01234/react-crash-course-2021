f08717a61814707678a0891d63e2903a7e39a904
unknown <yangd01234567@gmail.com>
2021-03-27 18:38:26 -0700

    initial react base

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello From React</h1>
+      <h2>First Template</h2>
+    </div>
+  );
+}
+
+export default App;
