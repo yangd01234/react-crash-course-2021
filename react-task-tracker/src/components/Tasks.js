e130134876c2c9ea401ff9accdab23b3d7d88967
unknown <yangd01234567@gmail.com>
2021-03-28 10:47:00 -0700

    added states, hooks, and sub components

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,23 +1,12 @@
-const tasks = [
-    {
-        id: 1,
-        text: "task1",
-        day: "Feb 5th at 2:30PM",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "task2",
-        day: "Feb 6th at 2:30PM",
-        reminder: true,
-    }
-]
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
+
     return (
+
         <>
         {tasks.map((task) => (
-            <h3 key={task.id}>{task.text}</h3>
+            <Task key={task.id} task={task} />
             ))}
         </>
     )
